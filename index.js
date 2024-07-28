490c21c329e4413156babc921656d2c74a41650e
CharanjitBK <charanbk9739@gmail.com>
2024-07-28 22:31:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// TEMPERATURE CONVERSION PROGRAM
+
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+
+let temp;
+
+function convert() {
+
+    if (toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp + " F"
+    }
+    else if (toCelsius.checked) {
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp + " C"
+    }
+    else {
+        result.textContent = "Select a unit";
+    }
+}
